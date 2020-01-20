35470250cf6eb8bae3de33a3aee2e4d60990dfa8
yaqee <2536085831.com>
2020-01-20 23:04:16 +0800

    创建两个文件

---
+++ src/main/java/stash/TestFile1.java
@@ -0,0 +1,7 @@
+package stash;
+
+public class TestFile1 {
+    public void firstFile() {
+        System.out.println("change first file.");
+    }
+}
---
+++ src/main/java/stash/TestFile2.java
@@ -0,0 +1,7 @@
+package stash;
+
+public class TestFile2 {
+    public void secondFile(){
+        System.out.println("change second file.");
+    }
+}
