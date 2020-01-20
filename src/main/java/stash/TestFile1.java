894cbf230449420ccbf7fdaf50f06cf4934d619c
yaqee <2536085831.com>
2020-01-20 23:17:16 +0800

    修改文件

--- src/main/java/stash/TestFile1.java
+++ src/main/java/stash/TestFile1.java
@@ -3,5 +3,10 @@ package stash;
 public class TestFile1 {
     public void firstFile() {
         System.out.println("change first file.");
+
+    }
+
+    public void changeInFirst() {
+        System.out.println("change in first.");
     }
 }
--- src/main/java/stash/TestFile2.java
+++ src/main/java/stash/TestFile2.java
@@ -4,4 +4,8 @@ public class TestFile2 {
     public void secondFile(){
         System.out.println("change second file.");
     }
+
+    public void changeInFirst() {
+        System.out.println("change in first.");
+    }
 }
